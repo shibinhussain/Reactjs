dff653d2bc5c2a294e47daf1ee71fc47dea0e069
Shibin Hussain <shibinhussain82@gmail.com>
2021-08-25 16:52:29 +0530

    fetch error

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -4,22 +4,33 @@ import BlogList from './BlogList';
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then((res) => {
+        if (!res.ok) {
+          throw Error('couldnt fetch the data');
+        }
         return res.json();
       })
       .then((data) => {
         setBlogs(data);
         setIsPending(false);
+        setError(null)
+      })
+      .catch((err) => {
+        setBlogs(null)
+        setIsPending(false);
+        setError(err.message);
       });
   }, []);
 
   return (
     <div className="home">
-      {isPending && <div>Loading...</div>}
-      {blogs && <BlogList blogs={blogs} />}
+      { error && <div>{error}</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
     </div>
   );
 };
