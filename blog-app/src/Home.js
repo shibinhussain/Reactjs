d585859efa7b019726245d7dd2abcde95569612c
Shibin Hussain <shibinhussain82@gmail.com>
2021-08-25 13:48:34 +0530

    json & useeffect

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -1,21 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'loresm ipsum...', author: 'amar', id: 1 },
-    { title: 'Welcome party', body: 'loresm ipsum...', author: 'joshi', id: 2 },
-    { title: 'Web dev tips', body: 'loresm ipsum...', author: 'anand', id: 3 },
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
+  useEffect(() => {
+    fetch('http://localhost:8000/blogs')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} handleDelete={handleDelete} />
+      {blogs && <BlogList blogs={blogs} handleDelete={handleDelete} />}
     </div>
   );
 };
