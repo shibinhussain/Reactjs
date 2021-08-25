4afd59e55a681f65f9a60575c471140ba1fe5e10
Shibin Hussain <shibinhussain82@gmail.com>
2021-08-25 14:34:52 +0530

    loading msg

--- blog-app/src/Home.js
+++ blog-app/src/Home.js
@@ -3,6 +3,7 @@ import BlogList from './BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
@@ -11,11 +12,13 @@ const Home = () => {
       })
       .then((data) => {
         setBlogs(data);
+        setIsPending(false);
       });
   }, []);
 
   return (
     <div className="home">
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} />}
     </div>
   );
