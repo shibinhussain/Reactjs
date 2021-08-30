273d51e26689937fb46a0351618929de08ec838e
Shibin Hussain <shibinhussain82@gmail.com>
2021-08-30 10:23:28 +0530

    task-tracker commit 1

---
+++ task-tracker/src/App.js
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>heloo</h1>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
