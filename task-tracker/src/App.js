d005e5aeb20ed9158c32c24ffc2e517148f11f63
Shibin Hussain <shibinhussain82@gmail.com>
2021-08-30 15:06:11 +0530

    tasks and task comp

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,9 +1,33 @@
 import Header from './components/Header';
+import { useState } from 'react';
+
+import Tasks from './components/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Sep 5th at 2.30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Sep 6th at 1.30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Shopping',
+      day: 'Sep 5th at 2.30pm',
+      reminder: false,
+    },
+  ]);
   return (
     <div className="container">
       <Header title="Task tracker" />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
