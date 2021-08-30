e4c31d9902be8c8060aa3fb635e089f06d463550
Shibin Hussain <shibinhussain82@gmail.com>
2021-08-30 16:17:32 +0530

    Addtask

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,8 +1,7 @@
 import Header from './components/Header';
 import { useState } from 'react';
-
+import AddTask from './components/AddTask';
 import Tasks from './components/Tasks';
-import { FaLevelDownAlt, FaLyft } from 'react-icons/fa';
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -25,6 +24,11 @@ function App() {
       reminder: false,
     },
   ]);
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
 
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id != id));
@@ -32,15 +36,16 @@ function App() {
 
   const toggleReminder = (id) => {
     setTasks(
-      tasks.map((task) => task.id === id
-        ? { ...task, reminder: !task.reminder }
-        : task
-    ));
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
   };
 
   return (
     <div className="container">
       <Header title="Task tracker" />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
