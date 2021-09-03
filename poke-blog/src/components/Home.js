8a93a3e995d16b763e740202615a963783f9ac3a
Shibin Hussain <shibinhussain82@gmail.com>
2021-09-03 14:03:42 +0530

    axios get - home

--- poke-blog/src/components/Home.js
+++ poke-blog/src/components/Home.js
@@ -1,27 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>
-        So how did the classical Latin become so incoherent? According to
-        McClintock, a 15th century typesetter likely scrambled part of Cicero's
-        De Finibus in order to provide placeholder text to mockup various fonts
-        for a type specimen book. It's difficult to find examples of lorem ipsum
-        in use before Letraset made it popular as a dummy text in the 1960s,
-        although McClintock says he remembers coming across the lorem ipsum
-        passage in a book of old metal type samples. So far he hasn't relocated
-        where he once saw the passage, but the popularity of Cicero in the 15th
-        century supports the theory that the filler text has been used for
-        centuries. And anyways, as Cecil Adams reasoned, “[Do you really] think
-        graphic arts supply houses were hiring classics scholars in the 1960s?”
-        Perhaps. But it seems reasonable to imagine that there was a version in
-        use far before the age of Letraset. McClintock wrote to Before & After
-        to explain his discovery.
-      </p>
-    </div>
-  );
-};
+class Home extends Component {
+  state = {
+    posts: [],
+  };
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then((res) => {
+      this.setState({
+        posts: res.data.slice(0, 15),
+      });
+    });
+  }
+  render() {
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map((post) => {
+        return (
+          <div className="post card" key={post.id}>
+            <span className="card-title">{post.title}</span>
+            <p>{post.body}</p>
+          </div>
+        );
+      })
+    ) : (
+      <div className="center">No Posts Yet</div>
+    );
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        {postList}
+      </div>
+    );
+  }
+}
 
 export default Home;
