2156fc2edda651d9f557056f7f0bfe56758b441b
Shibin Hussain <shibinhussain82@gmail.com>
2021-09-03 15:06:18 +0530

    Post C

--- poke-blog/src/App.js
+++ poke-blog/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Contact from './components/Contact';
 import About from './components/About';
+import Post from './components/Post';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route path="/contact" component={Contact} />
         <Route path="/About" component={About} />
+        <Route path="/:post_id" component={Post} />
       </div>
     </Router>
   );
