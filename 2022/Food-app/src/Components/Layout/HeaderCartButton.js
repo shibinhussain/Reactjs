d403badc9ccf5634121c8e487f55ed4368d4cfc9
Shibin Hussain <shibinhussain82@gmail.com>
2022-06-07 16:10:56 +0530

    use effect - bump cart icon

--- 2022/Food-app/src/Components/Layout/HeaderCartButton.js
+++ 2022/Food-app/src/Components/Layout/HeaderCartButton.js
@@ -1,18 +1,39 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const [buttonHighlighted, setButtonHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+  const { items } = cartCtx;
+  const numberOfCartItems = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    buttonHighlighted ? classes.bump : ''
+  }`;
+
+  useEffect(() => {
+    if (cartCtx.length === 0) {
+      return;
+    }
+    setButtonHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setButtonHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
